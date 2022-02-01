cc954d934390a507c42455c8205fcdf0a90504bb
Alexandre <drumond.ard@gmail.com>
2022-02-01 06:21:21 -0300

    fim projeto

--- js/scripts.js
+++ js/scripts.js
@@ -108,11 +108,75 @@ $(function(){
             valor_total *= 0.95;
         }
 
-        console.log('valor total: ', valor_total);
-        console.log('parametros: ', parametros);
+        window.setTimeout(function(){  
+            
+            // Com formato HTML
+            var id_gola = "#" + parametros.gola;
+            $('#result_gola').html( $(id_gola).html());
+
+            var id_estampa = "option[value='" + parametros.estampa + "']";
+            $('#result_estampa').html( $(id_estampa).html());
+
+            var id_qualidade = "#" + parametros.qualidade;
+            $('#result_qualidade').html( $(id_qualidade).html());
+
+            var id_cor = "#" + parametros.cor;
+            $('#result_cor').html( $(id_cor).html());
+            
+            var id_embalagem = "option[value='" + parametros.embalagem + "']";
+            $('#result_embalagem').html( $(id_embalagem).html());
+
+            // $('#result_gola').html(parametros.gola);                   // não busca o formato HTML
+            // $('#result_estampa').html(parametros.estampa);             // não busca o formato HTML
+            // $('#result_qualidade').html(parametros.qualidade);         // não busca o formato HTML
+            // $('#result_cor').html(parametros.cor);                    // não busca o formato HTML
+            // $('#result_embalagem').html(parametros.embalagem);        // não busca o formato HTML
+            $('#result_quantidade').html(parametros.quantidade);
+            $('#valor-total').html(valor_total.toLocaleString('pt-BR',{ minimumFractionDigits: 2, maximumFractionDigits: 2}) );
+            $('#foto-produto').attr("src", foto);
+
+        // console.log('valor total: ', valor_total);
+        // console.log('parametros: ', parametros);
         $('.refresh-loader').hide();
+    },1000);
     }
     
+    function atualizar_campos(parametros){
+    //cor
+    $('#cor').children().removeClass("selected");
+    var id_cor = "#" + parametros.cor;
+    $(id_cor).addClass("selected");
+
+    //gola
+    $('#gola').children().removeClass("selected");
+    var id_gola = "#" + parametros.gola;
+    $(id_gola).addClass("selected");
+
+    //qualidade
+    $('#qualidade').children().removeClass("selected");
+    var id_qualidade = "#" + parametros.qualidade;
+    $(id_qualidade).addClass("selected");
+
+    //estampa
+    $("#estampa").val(parametros.estampa);
+
+     //embalagem
+     $("#embalagem").val(parametros.embalagem);
+
+    //quantidade
+    $("#quantidade").val(parametros.quantidade);
+}
+
+    function atualizar_localStorage(parametros) {
+        window.localStorage.setItem("quantidade", parametros.quantidade)
+        window.localStorage.setItem("cor", parametros.cor)
+        window.localStorage.setItem("gola", parametros.gola)
+        window.localStorage.setItem("qualidade", parametros.qualidade)
+        window.localStorage.setItem("estampa", parametros.estampa)
+        window.localStorage.setItem("embalagem", parametros.embalagem)
+    }
+
+
     $(".option-filter div").click(function(){
         $(this).parent().children("div").removeClass("selected");
         $(this).addClass("selected");
@@ -120,15 +184,54 @@ $(function(){
         var categoria = $(this).parent().attr('id');
         parametros_pesquisa[categoria] = $(this).attr('id');
 
+        atualizar_localStorage(parametros_pesquisa);
         atualizar_orcamento(parametros_pesquisa);
-    })
+    });
 
-    // Ao carregar a página
 
+$("select").change(function(){
+    var parametro_select = $(this).attr('id');
+    parametros_pesquisa[parametro_select] = $(this).val();
+
+    atualizar_localStorage(parametros_pesquisa);
+    atualizar_orcamento(parametros_pesquisa);
+});
+
+$('#quantidade').change(function(){
+    var parametro_input = $(this).attr('id');
+    parametros_pesquisa[parametro_input] = $(this).val();
+
+    atualizar_localStorage(parametros_pesquisa);
+    atualizar_orcamento(parametros_pesquisa);
+
+});
+
+
+
+    // Ao carregar a página verificando localStorage
+
+    if (window.localStorage["quantidade"]) {
+        parametros_pesquisa.qualidade = (window.localStorage["quantidade"]);
+    }
+    if (window.localStorage["cor"]) {
+        parametros_pesquisa.cor = (window.localStorage["cor"]);
+    }
+    if (window.localStorage["gola"]) {
+        parametros_pesquisa.gola = (window.localStorage["gola"]);
+    }
+    if (window.localStorage["qualidade"]) {
+        parametros_pesquisa.qualidade = (window.localStorage["qualidade"]);
+    }
+    if (window.localStorage["estampa"]) {
+        parametros_pesquisa.estampa = (window.localStorage["estampa"]);
+    }
+    if (window.localStorage["embalagem"]) {
+        parametros_pesquisa.embalagem = (window.localStorage["embalagem"]);
+    }
 
     // Verificar local storage e atualizar a variável parametros_pesquisa
 
-
+    atualizar_campos(parametros_pesquisa);
     atualizar_orcamento(parametros_pesquisa);
 });
 
